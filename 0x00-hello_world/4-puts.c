e484d7feeae08b23c23195947a1aa72581485765
Ataubo Mbakwa <ataubsm@gmail.com>
2022-09-08 16:05:07 -0700

    printing strings with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 /**
+  * main - Entry  point
   *
   * Return: Always 0 (success)
   */
@@ -8,7 +9,6 @@
 int main(void)
 {
 
-		puts("\"programming is like building a multilingual  puzzle");
-
-		return(0);
+	puts("\"programming is like building a multilingual  puzzle");
+	return (0);
 }
