e2976b504b5a9513545c893de795be614e0cbf00
Ataubo Mbakwa <ataubsm@gmail.com>
2022-09-09 15:23:11 -0700

    This program will assign a random number to the variable n each time it is executed

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+  *main - Entry point
+  *
+  *Return: Always ) (Success)
+
+/* */
+int main(void)
+{
+		int n;
+
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+
+					
+
+					return (0);
+}
