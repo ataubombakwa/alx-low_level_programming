6186b44f5c91f63906e4dc44877d9cdb214ad8a9
Ataubo Mbakwa <ataubsm@gmail.com>
2022-09-13 15:37:01 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+/**
+  * print_sign - function to check for a sign of a number
+  * @n: is int that will use for the argument of the function
+  * Return 0 Always
+  */
+int print_sign(int n)
+{
+	if n > 0
+	{
+	_putchar('+');
+	return (1);
+}
+	else if (n < 0)
+	{
+	_putchar('-')
+	return(-1);
+	}
+	else
+	}
+	_putchar('0');
+	return (0);
+	}
+
