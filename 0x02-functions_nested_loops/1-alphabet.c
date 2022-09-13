b9ba0c640d2506b3ad0229072b5f98f66a10f96a
Ataubo Mbakwa <ataubsm@gmail.com>
2022-09-13 12:37:25 -0700

    void print_alphabet(void);

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+/**
+  * print_alphabet - function to print abc
+  *
+  * Return: Always 0
+  */
+
+#include "main.h"
+
+void print_alaphabet(void)
+{
+	char c;
+
+	for (c = 'a'; <= 'z'; c++)
+	{
+
+		_putchar(c);
+	}
+_putchar('\n');
+
+	return(0);
