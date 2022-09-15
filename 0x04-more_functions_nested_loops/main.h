4460dfd32e3be2b5b03846dc50432189bcfa119c
Ataubo Mbakwa <ataubsm@gmail.com>
2022-09-15 16:13:13 -0700

    main.h functions library

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,7 +5,7 @@
  * File: main.h
  * Auth: Ataubo Mbakwa Anjoanei
  * Desc: Header file containing prototypes for all functions
- * used in the 0x03-more_functions_nested_loops directory.
+ * used in the 0x03-more_functions_nested_loops directory
  */
 
 int _putchar(char c);
