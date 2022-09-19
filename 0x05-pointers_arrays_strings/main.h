287bff946c0c8eec0473793292926f184e7b1af5
Ataubo Mbakwa <ataubsm@gmail.com>
2022-09-19 16:34:42 -0700

    main.h file with function prototypes

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,18 +2,18 @@
 #define MAIN_H
 
 /**
-  * File: main.h
-  * Author: Ataubo Mbakwa Anjoanei
-  * Description: Protoype functions used on the 
-  * in the 0x04-pointers_arrays_strings directory.
-  */
+ * File: main.h
+ * Auth: Ataubo Mbakwa Anjoanei
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x04-pointers_arrays_strings directory.
+ */
 
 #include <stdlib.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
+size_t _strlen(const char *str);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
