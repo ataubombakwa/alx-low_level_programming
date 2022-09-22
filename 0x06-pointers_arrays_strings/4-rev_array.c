d64e98376797a066930cd107f21144498d766a1f
Ataubo Mbakwa <ataubsm@gmail.com>
2022-09-22 05:50:57 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,7 +7,7 @@
 
 /**
  * reverse_array - Reverses the content of an array of integers.
- * @a: The array of integers to be reversed.
+ * @a: The array of integers to be reversed..
  * @n: The number of elements in the array.
  */
 void reverse_array(int *a, int n)
