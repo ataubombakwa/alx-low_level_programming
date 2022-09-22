9fd03cfa4268a73155c7852ed253534d9b87f9ef
Ataubo Mbakwa <ataubsm@gmail.com>
2022-09-22 06:02:58 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,7 @@
  * @dest: string to append to
  * @src: string to add
  *
- * Return: a pointer to the resulting string
+ * Return: a pointer to the resulting string.
  */
 char *_strcat(char *dest, char *src)
 {
