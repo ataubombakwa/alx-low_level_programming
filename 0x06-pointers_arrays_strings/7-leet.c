d9c427078ebab27203787f545152fa30f024b96f
Ataubo Mbakwa <ataubsm@gmail.com>
2022-09-22 05:57:27 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@
  * leet - Encodes a string to 1337.
  * @str: The string to be encoded.
  *
- * Return: A pointer to the encoded string.
+ * Return: A pointer to the encoded string...
  */
 char *leet(char *str)
 {
