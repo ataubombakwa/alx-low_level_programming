63a38d51ffe27cc2b36872de5069d1c67a6c8e0a
ataubombakwa <ataubsm@gmail.com>
2022-10-21 23:51:37 +0100

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * list_len - finds the number of elements in a linked list
- *@h: pointer to the list
+ * list_len - finds the number of elements in a linked list.
+ *@h: pointer to the list.
  *
- *Return: the number of elements
+ *Return: the number of elements.
 */
 
 size_t list_len(const list_t *h)
