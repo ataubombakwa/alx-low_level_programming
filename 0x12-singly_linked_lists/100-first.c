5b02b5014a9308aa10773735ba8f8470e8fb473c
ataubombakwa <ataubsm@gmail.com>
2022-10-22 00:00:46 +0100

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
-2#include <stdio.h>
+#include <stdio.h>
 
 void __attribute__((constructor)) hare(void);
 
 /**
  * hare - Prints a string before the
- *   main function is executed...
+ *   main function is executed.
  */
 void hare(void)
 {
