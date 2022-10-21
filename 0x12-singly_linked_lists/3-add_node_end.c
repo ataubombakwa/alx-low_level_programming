d20d98189c111c1c0faf2b6518d55adacd5a9f78
ataubombakwa <ataubsm@gmail.com>
2022-10-21 23:53:47 +0100

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * add_node_end - Adds a new node at the end of a list
- *@head: pointer to the head of the list
- *@str: string to be added to the list
+ * add_node_end - Adds a new node at the end of a list.
+ *@head: pointer to the head of the list.
+ *@str: string to be added to the list.
  *
  *Return: NULL in case of failure
  *or address or the element
