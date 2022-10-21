c27ca0e8a29f5cac6ba33e42808629450c50c9ea
ataubombakwa <ataubsm@gmail.com>
2022-10-21 23:50:27 +0100

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
-* print_list - prints all the elements of a list
+* print_list - prints all the elements of a list.
 *
-* @h:pointer to the list
+* @h:pointer to the list.
 *
-*Return: the number of nodes
+*Return: the number of nodes.
 */
 
 size_t print_list(const list_t *h)
