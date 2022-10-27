81e37999eae2a700b6141064398c4dd50dad87d7
ataubombakwa <ataubsm@gmail.com>
2022-10-27 01:14:47 +0100

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,23 +1,25 @@
-#include "lists.h"
+#include <stdlib.h>
 #include <stdio.h>
-
+#include "lists.h"
 /**
- * print_listint - Print elements of a singly linked list.
- * @h: Pointer to a list.
- * Return: Integer.
+ * print_listint -print elements of the list. An integer.
+ *Description: Function that prints content of a single linked list
+ * @h: pointer to a structure typedef list_t
+ * Return: the number of nodes
  **/
-
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *tp;
-	unsigned int cntr = 0;
-
-	tp = h;
-	while (tp)
+/*temp is a listint_t type. It is a structure*/
+	const listint_t *temp = h;
+	size_t numNodes;
+/*
+ * if temp is null, it means it is in the last node. For that reason the
+ * condition is: while temp diferent from NULL
+ */
+	for (numNodes = 0; temp != NULL; numNodes++)
 	{
-		printf("%d\n", tp->n);
-		cntr++;
-		tp = tp->next;
+		printf("%d\n", temp->n);
+		temp = temp->next; /*temp will point to the next node*/
 	}
-	return (cntr);
+	return (numNodes);
 }
