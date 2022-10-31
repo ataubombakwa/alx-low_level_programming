887d88266e68122a610bd15966bfdc25f633fbd3
ataubombakwa <ataubsm@gmail.com>
2022-10-31 23:48:01 +0100

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,48 @@
 #include "main.h"
 /**
- *read_textfile - reads a text file and prints it to the
- *POSIX standard output
- *@filename:pointer to file description
- *@letters:number of letters it should read and print
- *Return:actual no. of letters it could read and print
- */
+ * read_textfile -.
+ *Description: function that reads a text file and prints it to the std output
+ * @filename: contain the name of the file to read
+ * @letters: number of letters to read
+ * Return: the number of char writed.
+ **/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, bytes;
+	int fd;
 	char *buf;
+	ssize_t wr = 0, rd = 0;
 
-/*check if pointer is NULL or letters < 1*/
 	if (filename == NULL)
 		return (0);
-	if (letters < 1)
-		return (0);
 
-	fd = open(filename, O_RDONLY);
-/*if the process is not successful*/
+	fd = open(filename, O_RDONLY); /*open file an return file descriptor*/
+
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	bytes = read(fd, buf, letters);
-	buf[bytes + 1] = '\0';
 
-	write(STDOUT_FILENO, buf, bytes);
+	buf = malloc(sizeof(char) * letters);
+
+	if (buf == NULL)
+		return (0);
+/*we use file descriptor to do thigns to the file (read or writwe, etc)*/
+/*from disc to ram, the letters are transfered to my buf in ram*/
+	rd = read(fd, buf, letters);
+	if (rd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+/*copy from ram to stdoutput not in disk, stdout:*/
+/*special file that point to the terminal*/
+	wr = write(STDOUT_FILENO, buf, rd);
+
+	if (wr == -1)
+	{
+		free(buf);
+		return (0);
+	}
 	close(fd);
 	free(buf);
-	return (bytes);
+
+	return (wr);
 }
